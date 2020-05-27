c98a9cdef7ff3c177e718c442c0b46f398ef9512
Juliano <julianoferrarigianlupi@gmail.com>
2020-05-27 13:47:14 -0400

    removed copying .pyc from source simulation files

--- tool_maker.py
+++ tool_maker.py
@@ -8,126 +8,92 @@ import os
 import shutil
 import sys
 
-
-
-
 num_args = len(sys.argv)
 if num_args != 4:
-    print( "Usage: python %s <short tool name> <full-path-to-new-directory> <full-path-to-your-cc3d-project>"%(os.path.basename(__file__)) )
-    
+    print("Usage: python %s <short tool name> <full-path-to-new-directory> <full-path-to-your-cc3d-project>" % (
+        os.path.basename(__file__)))
+
     sys.exit(1)
-
-
-
-
 
 shortName = sys.argv[1]
 
-if ((len(shortName) > 15) or 
-    (len(shortName) < 3) or 
-    (' ' in shortName) or
-    (not shortName.isalnum())):
+if ((len(shortName) > 15) or
+        (len(shortName) < 3) or
+        (' ' in shortName) or
+        (not shortName.isalnum())):
     print("Invalid shortname:\n  ")
     print("Tool name should be unique and contain 3-15 \n alphanumeric characters, no spaces.")
     print("Once you register your tool, you cannot change its name, \n so be careful to pick a good one.")
-    
-    
+
     sys.exit(1)
 
 dest = sys.argv[2]
 source = sys.argv[3]
 
-
 if not os.path.isdir(source):
     raise Exception('Path of source not found:', source)
 else:
     if 'Simulation' not in os.listdir(source):
-        raise Exception('Simulation folder not found in:',source)
+        raise Exception('Simulation folder not found in:', source)
     else:
         found_cc3d = False
         for name in os.listdir(source):
             if name.endswith('.cc3d'):
                 found_cc3d = True
-                cc3d_file_name = name.replace('.cc3d','')
+                cc3d_file_name = name.replace('.cc3d', '')
         if not found_cc3d:
-            raise Exception('.cc3d file not found in:',source)
-        
+            raise Exception('.cc3d file not found in:', source)
 
 if not os.path.isdir(dest):
     raise Exception('Path of destination not found:', dest)
 
-
-
 print("\n\n STEP 1: copying your simulation files\n")
-tool_cc3d_files = os.path.join(dest,'main')
+tool_cc3d_files = os.path.join(dest, 'main')
 print(source, '-->', tool_cc3d_files)
 try:
-    shutil.copytree(source,tool_cc3d_files)
-except: 
+    shutil.copytree(source, tool_cc3d_files, ignore=shutil.ignore_patterns('*.pyc', '*.zip'))
+except:
     print("unable to copy")
-
 
 print("\n\n STEP 2: copying critical tool files\n")
 for name in os.listdir('.'):
     try:
-        if name[0] !='.':
+        if name[0] != '.':
             if os.path.isdir(name):
                 source_dir = name
-                dest_dir = os.path.join(dest,name)
+                dest_dir = os.path.join(dest, name)
                 print(source_dir, '-->', dest_dir)
-                shutil.copytree(source_dir,dest_dir)
+                shutil.copytree(source_dir, dest_dir)
             elif not (name == sys.argv[0] or name == "README.md" or name == "tool_maker.py"):
                 source_file = name
-                dest_file = os.path.join(dest,name)
+                dest_file = os.path.join(dest, name)
                 print(source_file, '-->', dest_file)
-                shutil.copy(source_file,dest_file)
+                shutil.copy(source_file, dest_file)
     except:
         print("unable to copy")
-
-
 
 print("\n\n STEP 3: renaming files and content\n")
 
 os.chdir(dest)
 
-
 with open('middleware/invoke', 'r') as f:
     new_text = f.read().replace("-t toolname", "-t " + shortName)
-    new_text = new_text.replace("@tool/bin/toolname.sh", "@tool/bin/"+shortName+".sh")
+    new_text = new_text.replace("@tool/bin/toolname.sh", "@tool/bin/" + shortName + ".sh")
 
 with open('middleware/invoke', 'w') as f:
     f.write(new_text)
 
-old_sh = os.path.join('bin','nh-cc3d-toolname.sh')
-new_sh = os.path.join('bin','nh-cc3d-'+shortName+'.sh')
+old_sh = os.path.join('bin', 'nh-cc3d-toolname.sh')
+new_sh = os.path.join('bin', 'nh-cc3d-' + shortName + '.sh')
 
 try:
-    shutil.move(old_sh,new_sh)
-    print('renamed',old_sh,new_sh)
+    shutil.move(old_sh, new_sh)
+    print('renamed', old_sh, new_sh)
 except:
-    print("couldn't rename, carying on")
+    print("couldn't rename, carrying on")
 
 with open(new_sh, 'r') as f:
-    new_text = f.read().replace("toolName",cc3d_file_name)
-    
-    
+    new_text = f.read().replace("toolName", cc3d_file_name)
 
-with open(new_sh,'w') as f:
+with open(new_sh, 'w') as f:
     f.write(new_text)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
