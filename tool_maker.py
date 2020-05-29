03ca54db1b263fea660383329f28f4db26bef10a
Juliano <julianoferrarigianlupi@gmail.com>
2020-05-29 10:50:32 -0400

    changed prints

--- tool_maker.py
+++ tool_maker.py
@@ -13,7 +13,7 @@ if num_args != 4:
     print("Usage: python %s <short-tool-name> <full-path-to-new-directory> <full-path-to-your-cc3d-project>" % (
         os.path.basename(__file__)))
     print("Short tool name should be unique and contain 3-15 \nalphanumeric characters, no spaces.")
-    print("Once you register your tool, you cannot change the short name name, \nso be careful to pick a good one.")
+    print("Once you register your tool, you cannot change the short tool name, \nso be careful to pick a good one.")
 
     sys.exit(1)
 
@@ -25,7 +25,7 @@ if ((len(shortName) > 15) or
         (not shortName.isalnum())):
     print("Invalid <short-tool-name>:\n  ")
     print("Short tool name should be unique and contain 3-15 \nalphanumeric characters, no spaces.")
-    print("Once you register your tool, you cannot change the short name name, \nso be careful to pick a good one.")
+    print("Once you register your tool, you cannot change the short tool name, \nso be careful to pick a good one.")
 
     sys.exit(1)
 
